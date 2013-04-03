a610583f9ab764eca695a40b225966304d5e365c
Cristian Drincu <cristidrincu@gmail.com>
2013-04-03 16:30:06 +0300

    add aplications and sections content tab

--- public_html/js/tabs_navi.js
+++ public_html/js/tabs_navi.js
@@ -1,7 +1,13 @@
 $(document).ready(function(){
-    $('ul#tabsNavi a').click(function (e) {
-        e.preventDefault();
-        $(this).tab('show');
+//    $('ul#tabsNavi a').click(function (e) {
+//        e.preventDefault();
+//        $(this).tab('show');
+//    });
+    
+    $('ul#mainMenuNav li').click(function(){
+        var activeLinkMainMenu = $('ul#mainMenuNav li.active');
+        $(activeLinkMainMenu).removeClass('active');
+        $(this).addClass('active');
     });
 });
 
